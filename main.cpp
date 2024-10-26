b985d80b3880cb019fbbdc081df1a10bbcfee3b4
Skylar Robinson <skylardawn150@gmail.com>
2024-10-25 20:55:42 -0700

    Commit 3

--- main.cpp
+++ main.cpp
@@ -13,15 +13,18 @@ COMSC 210 | Lab 25 | Skylar Robinson | IDE Used: Eclipse
 using namespace std;
 using namespace std::chrono;
 
-const int TEST_NUM = 4;
-//parallel array to test results
-const string tests [TEST_NUM] = {"Read", "Sort", "Insert", "Delete"};
+const int RACE_NUM = 4;
+//parallel array to race results
+const string races [RACE_NUM] = {"Read", "Sort", "Insert", "Delete"};
 
 int main() {
 	//initialize vector, list, and set
 	vector<string> vector;
 	list<string> list;
 	set<string> set;
+
+	//initialize results arrays
+	int vectorRs[RACE_NUM], listRs[RACE_NUM], setRs[RACE_NUM];
 
     //open file
 	ifstream in("codes.txt");
@@ -33,12 +36,31 @@ int main() {
 	while (in >> temp)
 		vector.push_back(temp);
 
-	auto end = high_resolution_clock::now();
+	auto stop = high_resolution_clock::now();
+	vectorRs[0] = duration_cast<milliseconds>(stop - start);
+
+	//read to list
+	start = high_resolution_clock::now();
+
+	string temp;
+	while (in >> temp)
+		list.push_back(temp);
+
+	stop = high_resolution_clock::now();
+	listRs[0] = duration_cast<milliseconds>(stop - start);
+
+	//read to set
+	start = high_resolution_clock::now();
+
+	string temp;
+	while (in >> temp)
+		set.insert(temp);
+
+	stop = high_resolution_clock::now();
+	setRs[0] = duration_cast<milliseconds>(stop - start);
 
     return 0;
 }
-
-
 
 /* syntax examples:
 auto start = high_resolution_clock::now()
