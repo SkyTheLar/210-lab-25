c8a704f619f837243fb36cbb0a7dd99995fc3067
Skylar Robinson <skylardawn150@gmail.com>
2024-10-25 20:45:40 -0700

    Commit 2

--- main.cpp
+++ main.cpp
@@ -5,20 +5,40 @@ COMSC 210 | Lab 25 | Skylar Robinson | IDE Used: Eclipse
 ******************************************************/
 
 #include <iostream>
+#include <vector>
+#include <list>
+#include <set>
 #include <fstream>
 #include <chrono>
 using namespace std;
 using namespace std::chrono;
 
-int main() {
-    //open file
+const int TEST_NUM = 4;
+//parallel array to test results
+const string tests [TEST_NUM] = {"Read", "Sort", "Insert", "Delete"};
 
+int main() {
+	//initialize vector, list, and set
+	vector<string> vector;
+	list<string> list;
+	set<string> set;
+
+    //open file
+	ifstream in("codes.txt");
+
+	//read to vector
 	auto start = high_resolution_clock::now();
 
+	string temp;
+	while (in >> temp)
+		vector.push_back(temp);
 
+	auto end = high_resolution_clock::now();
 
     return 0;
 }
+
+
 
 /* syntax examples:
 auto start = high_resolution_clock::now()
