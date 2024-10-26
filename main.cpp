9f76e237585a016241e83fb61ef4c2f17fe6f42e
Skylar Robinson <skylardawn150@gmail.com>
2024-10-25 20:35:37 -0700

    Commit 1

---
+++ main.cpp
@@ -0,0 +1,28 @@
+/*****************************************************
+
+COMSC 210 | Lab 25 | Skylar Robinson | IDE Used: Eclipse
+
+******************************************************/
+
+#include <iostream>
+#include <fstream>
+#include <chrono>
+using namespace std;
+using namespace std::chrono;
+
+int main() {
+    //open file
+
+	auto start = high_resolution_clock::now();
+
+
+
+    return 0;
+}
+
+/* syntax examples:
+auto start = high_resolution_clock::now()
+auto end = high_resolution_clock::now()
+auto duration = duration_cast<milliseconds>(end - start)
+duration.count() references elapsed milliseconds
+*/
